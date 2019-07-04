2075c427bcd1711f666266944b21a399bacfbdb0
Omar Elawady <omarelawady1998@gmail.com>
2019-07-04 12:26:14 +0200

    Add docstrings

--- cache_table.py
+++ cache_table.py
@@ -2,15 +2,42 @@ from collections import deque
 from collections import MutableMapping
 import time
 class EntryTime:
+    """A class to store the entry time of the key along with the key"""
     def __init__(self, key, insertion_time):
+        """Base of EntryTime
+
+        Arguments:
+            key {object} -- the key
+            insertion_time -- the time it was inserted in in milliseconds
+        """
         self.key = key
         self.insertion_time = insertion_time
 class CacheTable(MutableMapping): 
+    """A dict-like data structure in which the keys expire after a specific time"""
     def __init__(self, ttl):
+        """Base of CachTable
+
+        Arguments:
+            ttl -- time to live for the keys in milliseconds
+        
+        data          {dict}  -- The dict of the stored data
+        insertedTimes {deque} -- deque of EntryTime maintained in sorted order
+        """
         self.ttl = ttl
         self.data = {}
         self.insertedTimes = deque()
     def __getitem__(self, key):
+        """Gets the value of the key
+
+        Arguments:
+            key {object} -- the string representing the key
+
+        Raises:
+            KeyError -- if the key is expired or doesn't exist
+
+        Returns:
+            value of the key
+        """
         self.remove_expired()
         return self.data[key]
     def __setitem__(self, key, val):
@@ -20,17 +47,28 @@ class CacheTable(MutableMapping):
     def __delitem__(self, key):
         del self.data[key]
     def __len__(self):
+        self.remove_expired()
         return len(self.data)
     def __iter__(self):
         self.remove_expired()
         return self
     def get_time(self):
+        """Returns the milliseconds since some reference time(this actual time is not relevant)"""
         return int(round(time.time() * 1000))
     def remove_expired(self):
+        """Removes the expired key from the table and unmark their insertion time"""
         while len(self.insertedTimes) != 0 and self.is_expired(self.insertedTimes[0]):
                 del self.data[self.insertedTimes[0].key]
                 self.insertedTimes.popleft()
     def is_expired(self, entry):
+        """Checks whether a key is expired
+
+        Arguments:
+            entry {EntryTime} -- The objcet in which the insertion_time and the key name is stored
+
+        Returns:
+            bool -- true if the key is expired
+        """
         return self.get_time() - entry.insertion_time > self.ttl
 if __name__ == "__main__":
     data = CacheTable(2000)
--- cache_table_per_entry.py
+++ cache_table_per_entry.py
@@ -1,31 +1,90 @@
 from collections import MutableMapping
 import time
 class CacheTablePerEntry:
+    """This is a cache table which is a dict with diffrent expiration time for each key"""
     def __init__(self):
+        """Base for CacheTablePerEntry
+        
+        data        {dict} -- the dictionary in which the table is stored
+        removalTime {dict} -- the dictionary which marks the expiration time for each key
+        """
         self.data = {}
         self.removalTime = {}
+
     def get(self, key):
+        """Gets the value of the key if it exists and not expired
+
+        Arguments:
+            key {object} -- the name of the key
+
+        Raises:
+            KeyError -- if the key expired or doesn't exist
+
+        Returns:
+            val -- the value associated with the key
+        """
         self.remove_expired()
         return self.data[key]
+    
     def set(self, key, val, ttl):
+        """Adds or override the key with the given val to expire in ttl milliseconds
+
+        Arguments:
+            key {object} -- the name of the key
+            val {object} -- the value associated with the key
+            ttl {int}    -- time to live in milliseconds
+
+        """
         self.remove_expired()
         self.data[key] = val
         self.removalTime[key] = self.get_time() + ttl
+    
     def get_time(self):
+        """Gets the milliseconds with respect to some reference time(the reference is not important).
+
+        Returns:
+            int -- milliseconds since the reference time
+        """
         return int(round(time.time() * 1000))
+    
     def remove_expired(self):
+        """passes over all keys and revoves the one that passes its expiration time"""
         for key in list(self.removalTime.keys()):
             if self.is_expired(key):
                 self.remove_key(key)
+    
     def is_expired(self, key): 
+        """Checks whether the given key is expired
+        
+        Arguments:
+            key {objcet} -- the key to be checked
+
+        Returns:
+            bool -- true if the key passed its expiration time
+        """
         return self.get_time() > self.removalTime[key]
+    
     def remove_key(self, key):
+        """Removes the key from the dict and unmark its expiration time
+
+        Arguments:
+            key {object} -- the key to be removed
+        """
         del self.data[key]
         del self.removalTime[key]
     
     def exists(self, key):
+        """Checks whether key exists in the stored data
+
+        Arguments:
+            key {object} -- the key to be checked
+
+        Returns:
+            bool -- false if the key is not stored or expired, true otherwise
+        """
         self.remove_expired()
         return key in self.data
+
 if __name__ == "__main__":
     dic = CacheTablePerEntry()
     dic.set("expire", 1, 1000)
